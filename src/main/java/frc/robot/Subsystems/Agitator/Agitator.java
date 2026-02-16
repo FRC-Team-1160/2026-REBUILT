06190a95669e072ff2f34e0f5c7ba4cf1509d26e
LeonH011iz <lhollis@student.smusd.us>
2026-02-16 14:53:08 -0800

    Implement button status tracking in Agitator
    Added boolean to track button status and methods to change and retrieve its value.

--- src/main/java/frc/robot/Subsystems/Agitator/Agitator.java
+++ src/main/java/frc/robot/Subsystems/Agitator/Agitator.java
@@ -3,21 +3,38 @@ package frc.robot.Subsystems.Agitator;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Port;
 
 public class Agitator extends SubsystemBase {
     public SparkMax agitator_motor;
+    private boolean wasNotPressed;
 
     public Agitator() {
         agitator_motor = new SparkMax(Port.INTAKE_MOTOR, MotorType.kBrushless); //make the agitator mototr
+        wasNotPressed = true; //Sets the intial/defual value as true
     }
 
+    //Changes the status of a boolean wasNotPressed
+    public void changeAgitatorButtonStatus(){
+        if(wasNotPressed){
+            wasNotPressed = false;
+        }
+        else{
+            wasNotPressed = true;
+        }
+    }
+
+    public boolean getAgitatorButtonStatus(){
+        return wasNotPressed;
+    }
+    // Only runs if the boolean wasNotPressed is true
     public void setAgitatorVolts(double volts){
-        SmartDashboard.putNumber("agitator speed", volts);
-        agitator_motor.set(volts);
+        if(wasNotPressed){
+            SmartDashboard.putNumber("agitator speed", volts);
+            agitator_motor.set(volts);
+        }
     }
 
     @Override
@@ -25,4 +42,3 @@ public class Agitator extends SubsystemBase {
 
     }
 }
-
