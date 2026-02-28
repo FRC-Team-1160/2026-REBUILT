4f287226913385a31ef244a5f9b0664b16939a8b
BottomHat <tzee@student.smusd.us>
2026-02-28 15:18:15 -0800

    confused about auto
    made a test auto command for shooter

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -6,12 +6,25 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Subsystems.ExampleSubsystem;
+import frc.robot.Subsystems.DriveTrain.DriveTrain;
+import frc.robot.Subsystems.Shooter.Shooter;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
   public static Command exampleAuto(ExampleSubsystem subsystem) {
     return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+  }
+
+  public static Command shootAutoCommand(Shooter shooter, DriveTrain driveTrain) {
+    return new FunctionalCommand(
+      () -> shooter.runMotors(1), 
+      () -> {}, 
+      (interrupted) -> shooter.stopMotors(), 
+      () -> Commands.waitSeconds(2).isFinished()  // change all this stuff later
+    );
   }
 
   private Autos() {
